f800ed27842e9b391e8d399ea861427216035701
Gethmi-Rathnayaka <gethmisrathnayaka@gmail.com>
2025-08-24 12:20:09 +0530

    enemy death sound

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,10 +4,12 @@ public class EnemyHealth : MonoBehaviour
 {
     public int ehealth = 30;
     private Animator animator;
+    private AudioSource audioSource;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void EnemyTakeDamage(int dmg)
@@ -23,7 +25,22 @@ public class EnemyHealth : MonoBehaviour
 
     private void Die()
     {
+        // Play death animation
         animator.SetTrigger(AnimationStrings.EnemyDeath);
-        Destroy(gameObject, 0.5f); // Delay to allow death animation to play
+
+        // Disable collisions
+        Collider2D col = GetComponent<Collider2D>();
+        if (col != null) col.enabled = false;
+
+        // Hide sprite
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null) sr.enabled = false;
+
+        // Play death sound
+        if (audioSource != null) audioSource.Play();
+
+        // Optional: destroy after audio finishes
+        Destroy(gameObject, audioSource.clip.length);
     }
+
 }
